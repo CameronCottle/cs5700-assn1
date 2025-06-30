b6426641a245c5fc6751d2f560f6111cdec3b98e
ccottle <ramm@gmail.com>
2025-06-30 17:23:34 -0400

    implemented the rect and square classes

--- src/main/kotlin/Rectangle.kt
+++ src/main/kotlin/Rectangle.kt
@@ -1,2 +1,31 @@
-class Rectangle {
+import kotlin.math.abs
+
+// open class to allow square to extend it
+open class Rectangle(private val pointA: Point, private val pointB: Point) : Shape {
+
+    init {
+        val width = abs(pointB.getX() - pointA.getX())
+        val height = abs(pointB.getY() - pointA.getY())
+        require(width != 0.0 && height != 0.0) {
+            "Rectangle cannot have width or height of 0."
+        }
+    }
+
+    // get the area of the rectangle
+    fun getArea(): Double {
+        val width = abs(pointB.getX() - pointA.getX())
+        val height = abs(pointB.getY() - pointA.getY())
+        return width * height
+    }
+
+    // move the rectangle
+    override fun move(dx: Double, dy: Double) {
+        pointA.move(dx, dy)
+        pointB.move(dx, dy)
+    }
+
+    // get the defining points of the rectangle
+    override fun getAttr(): List<Point> {
+        return listOf(pointA.clone(), pointB.clone())
+    }
 }
--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,2 +1,11 @@
-class Square {
+import kotlin.math.abs
+
+// extending the rectangle class, which inherits the Shape interface
+class Square(pointA: Point, pointB: Point) : Rectangle(pointA, pointB) {
+
+    init {
+        val width = abs(pointB.getX() - pointA.getX())
+        val height = abs(pointB.getY() - pointA.getY())
+        require(width == height) { "Square must have equal width and height." }
+    }
 }
